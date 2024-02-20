5ccafa1b7959fc0fe810641c9e7613e7db4440b1
rizata <rizalta00@gmail.com>
2024-02-20 12:12:03 +0530

    ex 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,34 @@
 import { useState } from 'react';
 
+const Statistics = ({ good, neutral, bad }) => {
+  if ((good + neutral + bad) === 0) {
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    );
+  }
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>
+        good {good}
+        <br />
+        neutral {neutral}
+        <br />
+        bad {bad}
+        <br />
+        all {good + neutral + bad}
+        <br />
+        average {(good - bad) / (good + neutral + bad)}
+        <br />
+        positive {good * 100.0 / (good + neutral + bad)} %
+      </p>
+    </>
+  );
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -11,16 +40,9 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      <p>
-        good {good}
-        <br />
-        neutral {neutral}
-        <br />
-        bad {bad}
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
+  );
 }
 
 export default App;
