e301a066cbd4e0753af9aacdc7d3b3d47455ecef
rizata <rizalta00@gmail.com>
2024-02-26 11:53:05 +0530

    ex 2.1-2.5

---
+++ part2/courseinfo/src/Course.jsx
@@ -0,0 +1,35 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return (
+    <strong>total of {sum} exercises</strong>
+  )
+}
+  
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+
+const Content = ({ parts }) => 
+  <>
+    {parts && parts.map((part, index) => (
+      <Part part={part} key={index}/>
+    ))}     
+  </>
+
+const Course = ({ course }) => {
+	const { name, parts } = course;
+	return (
+		<div>
+			<Header course={name} />
+			<Content parts={parts} />
+			<Total parts={parts} />
+		</div>
+	);
+}
+
+export default Course;
