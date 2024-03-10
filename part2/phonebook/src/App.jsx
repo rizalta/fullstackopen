870bee2ee968bf83fd1f335e165521785d9aea53
rizata <rizalta00@gmail.com>
2024-03-10 11:37:49 +0530

    added 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Filter from './Filter';
 import PersonForm from './PersonForm.jsx';
 import Persons from './Persons.jsx';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-      { name: 'Arto Hellas', number: '040-123456', id: 1 },
-      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => setPersons(response.data));
+  }, []);
   
   
-  return(
+  return (
     <div>
       <h2>Phonebook</h2>
       <Filter search={search} setSearch={setSearch} />
