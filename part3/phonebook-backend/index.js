3facb01ccf1a68e3f40a630ead1efb61316b2ff7
rizata <rizalta00@gmail.com>
2024-03-15 15:26:52 +0530

    fixed deletion

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -61,7 +61,10 @@ app.get("/api/persons/:id", (req, res) => {
 
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
-  const person =  persons.find(p => p.id = id);
+  const person =  persons.find(p => p.id === id);
+  if (!person) {
+    return res.status(404).json({ error: "Person not found" });
+  }
   persons = persons.filter(p => p.id !== person.id);
   res.status(200).json(person);
 });
